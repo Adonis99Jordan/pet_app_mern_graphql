bce5484ef84d5bc8b64741f3a66e793b712b06c9
TukuNoni71 <Adonisjzepeda@gmail.com>
2024-11-22 13:41:42 -0500

    script update

--- client/cypress.config.js
+++ client/cypress.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: "http://localhost:5173",
+    baseUrl: is_prod ? 'http://localhost:3333' : 'http://localhost:5173',
   },
 
   component: {
